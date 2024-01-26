380dd87ff7f2be23d995ac6408e6fc98541f6367
mrugesh <mrugeshlimbachiya123@gmail.com>
2024-01-27 02:11:50 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+# from functions import get_todos, write_todos
+import functions
+import time
+
+now = time.strftime("%b %d, %Y:%H:%M:%S")
+print(f"It is {now}")
+while True:
+    # Get user input and strip space chars from it
+    user_action = input("Enter add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith('add'):
+        todo = user_action[4:]
+
+        todos = functions.get_todos()
+
+        todos.append(todo + '\n')
+
+        functions.write_todos(todos)
+
+    elif user_action.startswith('show'):
+        todos = functions.get_todos('todos.txt')
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+    elif user_action.startswith('edit'):
+        try:
+            number = int(user_action[5:])
+            print(number)
+
+            number = number - 1
+
+            todos = functions.get_todos()
+
+            new_todo = input("Enter new todos: ")
+            todos[number] = new_todo + '\n'
+
+            functions.write_todos(todos)
+        except ValueError:
+            print('Your command is not valid.')
+        except IndexError:
+            print('There is no item with that number.')
+            continue
+
+    elif user_action.startswith('complete'):
+        try:
+            number = int(user_action[9:])
+            todos = functions.get_todos()
+
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            functions.write_todos(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
+        except IndexError:
+            print('There is no item with that number.')
+
+    elif user_action.startswith('exit'):
+        break
+
+    else:
+        print("Invalid Command Entered")
+
+print("Program finished successfully")
